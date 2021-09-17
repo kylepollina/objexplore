91819f53b7f0e2cdeaac9653095b3daa35cfe9d5
kyle pollina <>
2021-09-16 20:49:06 -0700

    Update to version 1

--- objexplore/objexplore.py
+++ objexplore/objexplore.py
@@ -16,7 +16,7 @@ from .help_layout import HelpLayout, HelpState
 from .overview_layout import OverviewLayout, OverviewState, PreviewState
 from .stack_layout import StackFrame, StackLayout
 
-version = "0.9.5"
+version = "1.0"
 
 # TODO methods filter
 # or just a type filter?
