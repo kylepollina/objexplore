f15caceee348c0d8b3e6be61babdc0b9406c9e9d
kyle pollina <>
2021-09-19 20:13:45 -0700

    Update version

--- objexplore/objexplore.py
+++ objexplore/objexplore.py
@@ -20,7 +20,7 @@ from .overview_layout import OverviewLayout, OverviewState, PreviewState
 from .stack_layout import StackFrame, StackLayout
 from .utils import is_selectable
 
-version = "1.3.1"
+version = "1.4.0"
 
 # TODO custom terminal class
 # TODO search filter
