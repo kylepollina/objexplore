982b919347aa640a268d7efef04d97e2df57de11
kyle pollina <>
2021-09-25 10:58:29 -0700

    Update version

--- objexplore/objexplore.py
+++ objexplore/objexplore.py
@@ -27,7 +27,7 @@ from .overview import Overview, OverviewState, PreviewState
 # TODO builtin frame/stack explorer? from objexplore import stackexplore
 
 
-version = "1.5.3"
+version = "1.5.4"
 console = Console()
 EDITOR = os.environ.get("EDITOR")
 
